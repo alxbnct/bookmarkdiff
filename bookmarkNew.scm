d7d35a40c7cf134471289e804d9e1f65ef5c8066
Yoseph Abraham <yoshissac6@gmail.com>
2022-11-23 14:38:29 +0800

    Use hash table to remove duplicate bookmarks.

--- bookmarkNew.scm
+++ bookmarkNew.scm
@@ -24,3 +24,28 @@
 	links)))
   (with-input-from-file filename
     (lambda () (html->txt (current-input-port))) #:text))
+
+;; Remove duplicate entries of two hash tables from the larger
+;; hash table and return it
+(define (hash-table-remove-duplicate h1 h2)
+  (define (start-remove)
+    (let ((hs1 (hash-table-size h1))
+          (hs2 (hash-table-size h2)))
+      (cond ((= hs1 hs2) (void))
+            ((> hs1 hs2) (remove-dup h1 h2))
+            ((< hs1 hs2) (remove-dup h2 h1)))))
+
+  (define (remove-dup larger-ht smaller-ht)
+   ;; (let ((ret (hash-table-copy larger-ht))	;This is equivalent as follows.
+    (let ((ret larger-ht)
+          (key-lst (hash-table-keys smaller-ht)))
+      (let lp ((lst key-lst))
+        (if (null? lst)
+            ret
+            (let ((key (car lst)))
+              ;; (print key)
+              (when (hash-table-exists? ret key)
+                  (hash-table-delete! ret key))
+              (lp (cdr lst)))))))
+
+  (start-remove))
